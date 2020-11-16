116c33c3ae4858c13f93c6a3582a4396c567eded
ajbain <ashleytiller@gmail.com>
2020-11-16 14:08:17 -0500

    Added ingredient starter class

---
+++ GroceryStoreAPI/Models/Ingredients.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GroceryStoreAPI.Models
+{
+    public enum DietType { GlutenFree, Vegan, Vegetarian, DairyFree}
+    public enum GroceryLocation { Frozen, Meat, Deli, Bakery, Produce, Dairy, Snacks, Condiments}
+    public class Ingredients
+    {
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+        public int UPC { get; set; }
+    }
+}
